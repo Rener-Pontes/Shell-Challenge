949e4bea23df6c2350d44cef1e5fd66fad7242aa
Rener Pontes <renercpontes@gmail.com>
2026-03-04 20:49:51 -0300

    Change how main is reading the inputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#include <cstring>
 #include <iostream>
+#include <print>
 #include <string>
-#include <string_view>
+
+#include "shell.hpp"
 
 int main() {
 	// Flush after every std::cout / std:cerr
@@ -14,14 +15,22 @@ int main() {
 		std::string input;
 		std::getline(std::cin, input);
 
-		if (auto found = input.find("exit"); found != std::string::npos) {
+		// println("\"{}\"", input.substr(0,4));
+
+		if (input.substr(0, 4) == "exit") {
 			return 0;
-		} else if (auto found = input.find("echo"); found != std::string::npos) {
-			std::string text = input.substr(found + strlen("echo "));
-			std::cout << text << std::endl;
+		} else if (input.substr(0, 4) == "echo") {
+			if (input.length() == 4) {
+				std::cout << std::endl;
+				continue;
+			}
+			std::string text = input.substr(4+1);
+			shell::builtins::echo(text);
+		} else if (input.substr(0, 4) == "type") {
+			if (input.size() == 4) continue;
+			shell::builtins::type(input.substr(4+1));
 		} else {
 			std::cout << input << ": command not found" << std::endl;
 		}
-
 	}
 }
